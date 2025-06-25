3a4801ba2cc22a0465d555dfc048208fb8cf7bb0
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2025-06-25 17:46:48 +0200

    KernelSU: sucompat toggle for non-kp
    Merge: https://github.com/tiann/KernelSU/pull/2506
    
    Co-Authored By: @backslashxx

--- KernelSU/kernel/sucompat.c
+++ KernelSU/kernel/sucompat.c
@@ -22,6 +22,10 @@
 
 extern void escape_to_root();
 
+#ifndef CONFIG_KSU_WITH_KPROBES
+static bool ksu_sucompat_non_kp __read_mostly = true;
+#endif
+
 static void __user *userspace_stack_buffer(const void *d, size_t len)
 {
 	/* To avoid having to mmap a page in userspace, just write below the stack
@@ -50,6 +54,12 @@ int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
 {
 	const char su[] = SU_PATH;
 
+#ifndef CONFIG_KSU_WITH_KPROBES
+	if (!ksu_sucompat_non_kp) {
+ 		return 0;
+ 	}
+#endif
+
 	if (!ksu_is_allow_uid(current_uid().val)) {
 		return 0;
 	}
@@ -70,6 +80,12 @@ int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
 {
 	// const char sh[] = SH_PATH;
 	const char su[] = SU_PATH;
+
+#ifndef CONFIG_KSU_WITH_KPROBES
+	if (!ksu_sucompat_non_kp) {
+ 		return 0;
+ 	}
+#endif
 
 	if (!ksu_is_allow_uid(current_uid().val)) {
 		return 0;
@@ -115,6 +131,12 @@ int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
 	const char sh[] = KSUD_PATH;
 	const char su[] = SU_PATH;
 
+#ifndef CONFIG_KSU_WITH_KPROBES
+	if (!ksu_sucompat_non_kp) {
+ 		return 0;
+ 	}
+#endif
+
 	if (unlikely(!filename_ptr))
 		return 0;
 
@@ -143,6 +165,12 @@ int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user,
 {
 	const char su[] = SU_PATH;
 	char path[sizeof(su) + 1];
+
+#ifndef CONFIG_KSU_WITH_KPROBES
+	if (!ksu_sucompat_non_kp){
+ 		return 0;
+ 	}
+#endif
 
 	if (unlikely(!filename_user))
 		return 0;
@@ -181,6 +209,13 @@ int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user,
 
 int ksu_handle_devpts(struct inode *inode)
 {
+
+#ifndef CONFIG_KSU_WITH_KPROBES
+	if (!ksu_sucompat_non_kp){
+ 		return 0;
+ 	}
+#endif
+
 	if (!current->mm) {
 		return 0;
 	}
@@ -288,6 +323,8 @@ void ksu_sucompat_init()
 	su_kps[1] = init_kprobe(SYS_FACCESSAT_SYMBOL, faccessat_handler_pre);
 	su_kps[2] = init_kprobe(SYS_NEWFSTATAT_SYMBOL, newfstatat_handler_pre);
 	su_kps[3] = init_kprobe("pts_unix98_lookup", pts_unix98_lookup_pre);
+#else
+	ksu_sucompat_non_kp = true;
 #endif
 }
 
@@ -297,5 +334,7 @@ void ksu_sucompat_exit()
 	for (int i = 0; i < ARRAY_SIZE(su_kps); i++) {
 		destroy_kprobe(&su_kps[i]);
 	}
+#else
+	ksu_sucompat_non_kp = false;
 #endif
 }

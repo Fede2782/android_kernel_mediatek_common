cb59bb41dae63e409aaa5b5b94d74fe2a55707ec
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2025-06-26 08:32:40 +0200

    Import minimal Samsung OSRC A346NKSU9DYE7 changes

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -3445,6 +3445,7 @@ static int sd_revalidate_disk(struct gendisk *disk)
 	struct scsi_disk *sdkp = scsi_disk(disk);
 	struct scsi_device *sdp = sdkp->device;
 	struct request_queue *q = sdkp->disk->queue;
+	const struct scsi_host_template *sht = sdp->host->hostt;
 	sector_t old_capacity = sdkp->capacity;
 	unsigned char *buffer;
 	unsigned int dev_max, rw_max;
@@ -3543,8 +3544,13 @@ static int sd_revalidate_disk(struct gendisk *disk)
 	 */
 	rw_max = min_not_zero(rw_max, sdp->host->opt_sectors);
 
-	/* Do not exceed controller limit */
-	rw_max = min(rw_max, queue_max_hw_sectors(q));
+	if (!strncmp(sht->name, "ufshcd", 6)) {
+		/* Set rw_max using hw_max when device is ufs */
+		rw_max = queue_max_hw_sectors(q);
+	} else {
+		/* Do not exceed controller limit */
+		rw_max = min(rw_max, queue_max_hw_sectors(q));
+	}
 
 	/*
 	 * Only update max_sectors if previously unset or if the current value

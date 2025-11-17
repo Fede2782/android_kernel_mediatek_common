dc030b351494345884a4dbb3c323161cf361c4f8
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2025-11-17 21:55:23 +0100

    Import minimal A346BXXUBEYI7 changes

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -32,6 +32,12 @@
 #include <linux/moduleparam.h>
 #include <linux/wakeup_reason.h>
 #include <linux/workqueue.h>
+
+#if IS_ENABLED(CONFIG_SEC_PM)
+#include <linux/regulator/machine.h>
+extern void sec_clock_debug_print_enabled(void);
+#endif /* CONFIG_SEC_PM */
+
 #include <trace/hooks/suspend.h>
 
 #include "power.h"
@@ -503,6 +509,12 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 					 suspend_stats.failed_devs[last_dev]);
 		goto Platform_finish;
 	}
+
+#if IS_ENABLED(CONFIG_SEC_PM)
+	regulator_show_enabled();
+	sec_clock_debug_print_enabled();
+#endif /* CONFIG_SEC_PM */
+
 	error = platform_suspend_prepare_late(state);
 	if (error)
 		goto Devices_early_resume;

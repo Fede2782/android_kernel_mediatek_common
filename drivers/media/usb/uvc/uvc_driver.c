2702e35603ec205c54f5c973215e20ae70020b09
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2025-12-04 16:31:58 +0100

    Import minimal A346BXXUBEYI7 changes

--- drivers/media/usb/uvc/uvc_driver.c
+++ drivers/media/usb/uvc/uvc_driver.c
@@ -249,7 +249,7 @@ static int uvc_parse_format(struct uvc_device *dev,
 	unsigned int width_multiplier = 1;
 	unsigned int interval;
 	unsigned int i, n;
-	u8 ftype;
+	u8 ftype = UVC_VS_UNDEFINED;
 
 	if (buflen < 4)
 		return -EINVAL;
@@ -391,7 +391,7 @@ static int uvc_parse_format(struct uvc_device *dev,
 	 * based formats have frame descriptors.
 	 */
 	while (ftype && buflen > 2 && buffer[1] == USB_DT_CS_INTERFACE &&
-	       buffer[2] == ftype) {
+	       buffer[2] == ftype && buffer[2] != UVC_VS_UNDEFINED) {
 		unsigned int maxIntervalIndex;
 
 		frame = &frames[format->nframes];
